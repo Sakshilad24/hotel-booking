828367ff1b3c60ca2b814d9df2a2b6bc8fbe56e2
Sakshilad24 <122873773+Sakshilad24@users.noreply.github.com>
2026-03-13 13:25:37 +0530

    Add files via upload

---
+++ client/src/components/RecommendedHotels.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import Title from './Title'
+import HotelCards from './HotelCards'
+import { useAppContext } from '../conext/AppContext'
+
+const RecommendedHotels = () => {
+
+  const { rooms, searchedCities } = useAppContext();
+  const [recommended, setRecommended] = useState([]);
+
+  const filterhotels = () => {
+    const filteredHotels = rooms.filter(room =>
+      searchedCities.includes(room.hotel.city)
+    );
+    setRecommended(filteredHotels);
+  }
+
+  useEffect(() => {
+    filterhotels();
+  }, [searchedCities, rooms]);
+
+  return (
+    recommended.length > 0 && (
+      <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
+
+        <Title
+          title="Recommended Hotels"
+          subTitle="Discover our handpicked selection of exceptional properties around the world, offering unparalleled luxury and unforgettable experiences"
+        />
+
+        <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
+          {recommended.slice(0, 4).map((room, index) => (
+            <HotelCards key={room._id} room={room} index={index} />
+          ))}
+        </div>
+
+      </div>
+    )
+  );
+};
+
+export default RecommendedHotels;              
